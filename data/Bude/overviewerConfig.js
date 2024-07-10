0352ecfefdae20656032bdc5f3b58aab2a26d8f2
Joseph Palmer <75081147+BlockBuilderJoe@users.noreply.github.com>
2024-07-10 09:50:01 +0100

    Overviewer map

---
+++ data/Bude/overviewerConfig.js
@@ -0,0 +1,72 @@
+var overviewerConfig = {
+    "tilesets": [
+        {
+            "imgextension": "png",
+            "north_direction": 0,
+            "poititle": "Markers",
+            "isOverlay": false,
+            "world": "world",
+            "center": [
+                0,
+                320,
+                0
+            ],
+            "bgcolor": "#1a1a1a",
+            "path": "world-lighting",
+            "showlocationmarker": true,
+            "base": "",
+            "maxZoom": 8,
+            "last_rendertime": 1720601051,
+            "name": "Overviewer Render (lighting)",
+            "spawn": [
+                0,
+                320,
+                0
+            ],
+            "minZoom": 0,
+            "zoomLevels": 8,
+            "lastrenderversion": 2,
+            "defaultZoom": 1
+        }
+    ],
+    "worlds": [
+        "world"
+    ],
+    "map": {
+        "north_direction": "lower-left",
+        "debug": false,
+        "controls": {
+            "mapType": true,
+            "pan": true,
+            "compass": true,
+            "spawn": true,
+            "zoom": true,
+            "overlays": true,
+            "coordsBox": true
+        },
+        "cacheTag": "1720601139"
+    },
+    "CONST": {
+        "tileSize": 384,
+        "LOWERLEFT": 3,
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "image": {
+            "bedMarker": "bed.png",
+            "defaultMarker": "signpost.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "signMarker": "signpost_icon.png",
+            "queryMarker": "markers/marker_location.png",
+            "spawnMarker": "markers/marker_home.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "2": "compass_lower-right.png",
+                "3": "compass_lower-left.png"
+            },
+            "spawnMarker2x": "markers/marker_home_2x.png"
+        },
+        "LOWERRIGHT": 2,
+        "UPPERRIGHT": 1
+    }
+};
