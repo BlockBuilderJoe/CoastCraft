ddcc0ba09ccd17f9b6310f56201ea6f525fe81cb
Joseph Palmer <75081147+BlockBuilderJoe@users.noreply.github.com>
2024-07-11 08:44:26 +0100

    added overview section to github

---
+++ overviewerConfig.js
@@ -0,0 +1,72 @@
+var overviewerConfig = {
+    "tilesets": [
+        {
+            "center": [
+                0,
+                320,
+                0
+            ],
+            "isOverlay": false,
+            "minZoom": 0,
+            "spawn": [
+                0,
+                320,
+                0
+            ],
+            "last_rendertime": 1720625998,
+            "base": "",
+            "north_direction": 0,
+            "world": "world",
+            "path": "world-lighting",
+            "defaultZoom": 1,
+            "zoomLevels": 8,
+            "bgcolor": "#1a1a1a",
+            "maxZoom": 8,
+            "showlocationmarker": true,
+            "imgextension": "png",
+            "lastrenderversion": 2,
+            "name": "Overviewer Render (lighting)",
+            "poititle": "Markers"
+        }
+    ],
+    "worlds": [
+        "world"
+    ],
+    "CONST": {
+        "LOWERRIGHT": 2,
+        "UPPERLEFT": 0,
+        "mapDivId": "mcmap",
+        "tileSize": 384,
+        "UPPERRIGHT": 1,
+        "image": {
+            "spawnMarker": "markers/marker_home.png",
+            "bedMarker": "bed.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "2": "compass_lower-right.png",
+                "3": "compass_lower-left.png"
+            },
+            "defaultMarker": "signpost.png",
+            "queryMarker": "markers/marker_location.png",
+            "signMarker": "signpost_icon.png"
+        },
+        "LOWERLEFT": 3
+    },
+    "map": {
+        "debug": false,
+        "north_direction": "lower-left",
+        "controls": {
+            "zoom": true,
+            "pan": true,
+            "spawn": true,
+            "overlays": true,
+            "mapType": true,
+            "compass": true,
+            "coordsBox": true
+        },
+        "cacheTag": "1720626090"
+    }
+};
